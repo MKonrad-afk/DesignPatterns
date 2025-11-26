656b15fdbb637420a59acce5e6a704641df46f89
Mkonrad-afk <s29778@pjwstk.edu.pl>
2025-11-26 14:55:26 +0100

    impleneted observer and implmenting swith mechanism with creating the databse

--- DesignPattern/obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
+++ DesignPattern/obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc25ffb73e148e7cee3a6d8561a924148b78feb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
